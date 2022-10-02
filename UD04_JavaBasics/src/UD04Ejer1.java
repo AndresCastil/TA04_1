d14194ff5c6868346745df7edba64dc62acfe1b7
AndresCastil <casaned@yahoo.com>
2022-10-03 01:50:03 +0200

    Primer commit

---
+++ UD04_JavaBasics/src/UD04Ejer1.java
@@ -0,0 +1,20 @@
+
+public class UD04Ejer1 {
+	public static void main(String args[]) {
+		
+		int num1=72;
+		int num2=8;
+		
+		System.out.println(num1+num2+" = Suma");
+		System.out.println(num1-num2+ " = Resta");
+		System.out.println(num1*num2+ " = Multiplicación");
+		System.out.println(num1/num2+ " = División");
+		System.out.println(num1%num2+ " = Módulo");
+		
+	}
+
+}
+
+	
+
+
